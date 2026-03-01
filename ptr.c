ef3aa10af669fba9d35964a039aa4dfb85d23d12
Larry B. Daniel <larry@refusetoown.com>
2026-03-01 18:06:03 -0500

    Refactor ptr.c to follow no imbalances and no singletons constraints

--- ptr.c
+++ ptr.c
@@ -4,14 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <time.h>
 #include "truth.h"
 
 #define SENSOR_DIM 1
-#define ACTION_COUNT 2 
-#define TRAINING_SAMPLES 496
-#define LEARNING_RATE 0.05
-#define EPOCHS 15
+#define ACTION_COUNT 2
 #define TRUTH 66
 
 int rnd(){
@@ -33,16 +29,6 @@ typedef struct {
 typedef struct {
     double v[SENSOR_DIM];
 } SensorVec;
-
-double random_double(double low, double high)
-{
-    return low + (high - low) * rnd();
-}
-
-static double randd(double a, double b) {
-
-	return random_double(a,b);
-}
 
 void model_init(Model *m) {
 	m->weights[0][0]=w1;
@@ -66,12 +52,6 @@ Action analyze_choose_action(const Model *m, const SensorVec *s) {
     return best_action;
 }
 
-// "Expert" rule-based label generator for training data
-Action expert_label(const SensorVec *s) {
-    if (s->v[0] > 0.8 ) return ACTION_TRUTH;
-    return ACTION_FALSE;
-}
-
 void simulate_sensor(SensorVec *s) {
     s->v[0] = (double) (rnd() );
 }
@@ -87,18 +67,11 @@ const char* action_name(Action a) {
 int main(void) {
     Model model;
     model_init(&model);
-    int quit=0;
 
-
-    // PREDICTION PHASE (fixed model, no updates)
-//    printf("--- Prediction Phase ---\n");
-//    for (int i = 0; i < 10; ++i) {
-        SensorVec s;
-	simulate_sensor(&s);
-        Action a = analyze_choose_action(&model, &s);
-	if  (a!=0) printf(" -> %s\n", action_name(a));
- //   }
-
+    SensorVec s;
+    simulate_sensor(&s);
+    Action a = analyze_choose_action(&model, &s);
+    if (a != 0) printf(" -> %s\n", action_name(a));
 
     return 0;
 }
